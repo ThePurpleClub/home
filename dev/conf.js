e908244b48afd87b4fb033d225d566d614469263
Po Tin Mak <60085635+potinmak@users.noreply.github.com>
2022-05-06 00:12:06 +0100

    Update conf.js

--- dev/conf.js
+++ dev/conf.js
@@ -1,6 +1,6 @@
 window.appConf = {
     author: 'React Drive CMS',
-    dashboardId: '1-on_GfmvaEcOk7HcWfKb8B6KFRv166RkLN2YmDEtDn4',
+    dashboardId: '1WYSZbWkOVJ1Hw7aetftM_j-j7n2_AOg-F6lWPyPP6qU',
     sendContactMessageUrlId:
         'AKfycbyL4vW1UWs4mskuDjLoLmf1Hjan1rTLEca6i2Hi2H_4CtKUN84d',
     shortname: 'easydrivecms',
