4401f3223122e5373da829967176d7e97cfb0022
Po Tin Mak <60085635+potinmak@users.noreply.github.com>
2022-05-16 16:51:15 +0100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -31,8 +31,6 @@ const App = () => {
             Page = Category
         } else if (pageName === 'articles') {
             Page = Article
-        } else if (pageName === 'memoriam') {
-            Page = Memoriam 
         }
         return Page
     }, [pageName])
