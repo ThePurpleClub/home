025d55f71bce05d3f4f22eb379ce5b584bc4eb20
Po Tin Mak <60085635+potinmak@users.noreply.github.com>
2022-05-18 21:41:30 +0100

    Update home.js

--- app/routes/home.js
+++ app/routes/home.js
@@ -6,7 +6,7 @@ import { PostsAndCategories } from '../components/layout/postsAndCategories.js'
 
 export const Home = ({ state, dispatch }) => html` <${Page}
     title="Harpy Eagle"
-    subtitle="The greatest eagle in the world"
+    subtitle="The largest and most powerful eagle in the world"
     description="This is Grayson, the harpy eagle, and you can find him at the World Center for Birds of Prey, Boise, Idaho, United States."
     sidebarImage=${prefixUriIfNeeded('/assets/harpy_eagle.jpg')}
     showLinks=${true}
