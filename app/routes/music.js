ac8cda687d8dbc5cc0c788af9ac47daaabffd9e8
Po Tin Mak <60085635+potinmak@users.noreply.github.com>
2022-05-19 13:20:01 +0100

    Update music.js

--- app/routes/music.js
+++ app/routes/music.js
@@ -56,7 +56,7 @@ export const Music = () => html`
         title="Prince Rogers Nelson"
         subtitle="The most prolific singer-songwriter and multi-instrumentalist in the world"
         description="Born and raised in Minneapolis, US, The Purple One is a pioneer of Minneapolis sound, and always have an exceptional work ethnic and passion in music craftmanship."
-        sidebarImage=${prefixUriIfNeeded('/assets/prince2.jpg')}
+        sidebarImage=${prefixUriIfNeeded('/assets/prince1.jpg')}
         showLinks=${true}
     >
         <div class="about-content">
