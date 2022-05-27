4d91bb26b64a5ef20a76f862282c8da94eeec27b
BaldEagleFlying <60085635+BaldEagleFlying@users.noreply.github.com>
2022-05-27 12:50:13 +0100

    Update menu.js

--- app/components/layout/menu.js
+++ app/components/layout/menu.js
@@ -13,7 +13,7 @@ export const Menu = ({ categories, articles, menuVisible }) => {
     }, [categories])
     return html` <style>
             .menu {
-                background-color: #66cc00; <!--#333 -->
+                background-color: #00cc00; <!--#333 -->
                 overflow: hidden;
                 z-index: 10;
                 display: block;
