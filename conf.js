021bb0446139f27a47f9842d89d5efbfd4dd8beb
BaldEagleFlying <60085635+BaldEagleFlying@users.noreply.github.com>
2022-05-29 23:58:13 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ window.appConf = {
     author: 'React Drive CMS',
     dashboardId: '1WYSZbWkOVJ1Hw7aetftM_j-j7n2_AOg-F6lWPyPP6qU',
     sendContactMessageUrlId:
-        '1y6jFwLO5QBblmauiUlYWBYUWXI0rRw2xJJNP-9CMNyU',
+        '1DH3W999Bpo5S17YKTG8OSmJe1PwF8xvDQQWsl9ZfVkEu6nZmJWUW4Zxm',
     shortname: 'easydrivecms',
     root: 'blogs',
     ipInfoToken: '6efb7a808a4857',
