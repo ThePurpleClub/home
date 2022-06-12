3987d19c72cf39f380bba7460c3daf8230700547
This is Po Tin Mak's repo <60085635+BaldEagleFlying@users.noreply.github.com>
2022-06-12 11:18:34 +0100

    Update conf.js

--- dev/conf.js
+++ dev/conf.js
@@ -2,7 +2,7 @@ window.appConf = {
     author: 'React Drive CMS',
     dashboardId: '1WYSZbWkOVJ1Hw7aetftM_j-j7n2_AOg-F6lWPyPP6qU',
     sendContactMessageUrlId:
-        '1DH3W999Bpo5S17YKTG8OSmJe1PwF8xvDQQWsl9ZfVkEu6nZmJWUW4Zxm',
+        '1YKpazzxBCeSXa2-cJzGCWgqdntWyhS3m4N5PlPWRcUqId54BO-aMBvbp',
     shortname: 'easydrivecms',
     ipInfoToken: '6efb7a808a4857',
     googleApiKey: 'AIzaSyDK37LHCky4_yIggqcKAYmFoXx6mryW7GM',
