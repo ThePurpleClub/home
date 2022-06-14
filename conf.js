d03d869f0a02f1e33e62f2e5d23685a6c8581ce1
This is Po Tin Mak's repo <60085635+BaldEagleFlying@users.noreply.github.com>
2022-06-14 16:49:30 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ window.appConf = {
     author: 'The Purple Hub',
     dashboardId: '1WYSZbWkOVJ1Hw7aetftM_j-j7n2_AOg-F6lWPyPP6qU',
     sendContactMessageUrlId:
-        '1YKpazzxBCeSXa2-cJzGCWgqdntWyhS3m4N5PlPWRcUqId54BO-aMBvbp',
+        'AKfycbxZlzysCLCQVqnHJjDwJJ-y_F7qroWw8TsohkyUoTHxZeQDb0xl2-8efiJqJ7lMFJBZ0w',
     shortname: 'easydrivecms',
     root: 'blogs',
     ipInfoToken: '0efb550629ccad',
