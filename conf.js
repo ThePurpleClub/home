c5f98ef7f2365475efc0e391078080e62b2e2ce0
This is Po Tin Mak's repo <60085635+ThePurpleClub@users.noreply.github.com>
2022-06-15 21:21:42 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -4,7 +4,7 @@ window.appConf = {
     sendContactMessageUrlId:
         'AKfycbzbSGucaLQx0fk21bXL6EPzP6GfY6Dp1FHwNoL2m_HeZyyfA7XYZLP5cOZMIi751TOoQQ',
     shortname: 'easydrivecms',
-    root: 'blogs',
+    root: 'home',
     ipInfoToken: '0efb550629ccad',
     googleApiKey: 'AIzaSyDK37LHCky4_yIggqcKAYmFoXx6mryW7GM',
 }
