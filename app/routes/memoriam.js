67ac888512b758402b5f5b1a7c8ad03bc50d4255
The Purple Club <60085635+ThePurpleClub@users.noreply.github.com>
2023-01-12 15:06:36 +0000

    Update memoriam.js

--- app/routes/memoriam.js
+++ app/routes/memoriam.js
@@ -73,7 +73,7 @@ export const Memoriam = () => html`
     </style>
     <${Page}
         title="Bald Eagle"
-        subtitle="Bald Eagle is the national symbol of the United States of America."
+        subtitle=""
         description="The fierce beauty and proud
         independence of this great bird aptly symbolizes the strength and freedom
         of America. -John F. Kennedy"
